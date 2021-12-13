e878fa567e158b0d78b855c92733a6965580fa93
jielinc <jielin.c@nus.edu.sg>
2021-12-13 16:04:00 +0800

    Yong-Siew-Toh-Conservatory-of-Music
    Add to repository

---
+++ data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-alice-lee-plaza",
+      "name": "Alice Lee Plaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.6273795253386254,
+        "pitch": 0.19026216777747607,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9923338310136938,
+          "pitch": -0.00021724532697575683,
+          "rotation": 10.995574287564278,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.249655176712764,
+          "pitch": -0.0670645752383372,
+          "title": "Yong Siew Toh Conservatory of Music",
+          "text": "Exterior facade of&nbsp;Yong Siew Toh Conservatory of Music"
+        }
+      ]
+    },
+    {
+      "id": "1-conservatory-foyer",
+      "name": "Conservatory Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.6490202758718668,
+        "pitch": 0.17832294620216516,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6925128416377415,
+          "pitch": 0.06929396820991052,
+          "rotation": 0,
+          "target": "0-alice-lee-plaza"
+        },
+        {
+          "yaw": -2.9574069781946477,
+          "pitch": 0.08341047437662397,
+          "rotation": 0.7853981633974483,
+          "target": "2-conservatory-lounge"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6490202758718668,
+          "pitch": 0.17832294620216516,
+          "title": "Yong Siew Toh Conservatory of Music 360 Web Tour",
+          "text": "Foyer of&nbsp;Yong Siew Toh Conservatory of Music"
+        }
+      ]
+    },
+    {
+      "id": "2-conservatory-lounge",
+      "name": "Conservatory Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.6300082588073153,
+        "pitch": 0.2259278619521794,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.348026593371479,
+          "pitch": 0.1116705707263641,
+          "rotation": 0,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9740949485624713,
+          "pitch": 0.1448963566711754,
+          "title": "Yong Siew Toh Conservatory of Music 360 Web Tour",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Yong Siew Toh Conservatory of Music Conservatory Lounge</span>"
+        }
+      ]
+    }
+  ],
+  "name": "Yong Siew Toh Conservatory of Music",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
